97df76ecf1bf8b20603c867a7cb2aacd0812b5a6
alexandru.v.serban@gmail.com <alexandru.v.serban@gmail.com@95414121-e61c-0a86-1be5-034fa2485955>
2012-05-23 10:18:46 +0000

    progress cmd line

--- myapp/chrome/content/main.js
+++ myapp/chrome/content/main.js
@@ -1,5 +1,192 @@
-function crap()
+function cmdArguments()
 {
-	var oMyPriority = Components.classes['@mozillazine.org/myapp/priority;1'].createInstance(Components.interfaces.nsISupportsPriority);
-	oMyPriority.adjustPriority(10);
+	
+}
+
+
+function config()
+{
+	this.cmd_args = window.arguments[0].QueryInterface(Components.interfaces.nsICommandLine);
+	this.tmp_rel_path = "../www/xulrunner-app-grab-preview/myapp/tmp";
+}
+
+
+
+var conf = new config();
+
+function app()
+{
+	this.url = "";	
+}
+
+var this_app = new app();
+
+function snapThat()
+{
+	this_app.url = conf.cmd_args.handleFlagWithParam("args",false);
+	this_app.
+	//printWithCanvas();
+}
+
+function outputFilePath(aMode) {
+  var path = (window.arguments && window.arguments[2])?
+               window.arguments[2] : "";
+  if (path)
+    return path;
+
+  var prefs = Components.classes["@mozilla.org/preferences;1"]
+                        .getService(Components.interfaces.nsIPrefBranch);
+  	
+  var fileLeaf = "";
+  try {
+    fileLeaf = prefs.getComplexValue("extensions.cmdlnprint.basefilename",
+                                      Components.interfaces.nsISupportsString)
+                    .data;
+    if (!fileLeaf)
+      fileLeaf = "snapshot.%EXT%";
+      
+    var title = getBrowser().contentDocument.title;
+    if (title.length > 32)
+      title = title.substring(0, 32);
+
+    fileLeaf = fileLeaf.replace("%HOST%", getBrowser().currentURI.host);
+    fileLeaf = fileLeaf.replace("%TITLE%", title);
+    fileLeaf = fileLeaf.replace("%DATE%", dateString());
+
+    var ext = "dat";
+
+    switch (aMode) {
+    case 1:
+      ext = "pdf";
+      break;
+    case 2:
+      ext = "png";
+      break;
+    case 3:
+      ext = "ps";
+      break;
+    }
+
+    fileLeaf = fileLeaf.replace("%EXT%", ext);
+
+    /* forbidden letters, as title can be contain any letters. */
+    while (/[\\\/\:\?\*\"\<\>\|]/.test(fileLeaf))
+      fileLeaf = fileLeaf.replace(/[\\\/\:\?\*\"\<\>\|]/g, "_");
+
+  }
+  catch(e) {
+    fileLeaf = "foo";
+  }
+
+  var file = Components.classes["@mozilla.org/download-manager;1"]
+                       .getService(Components.interfaces.nsIDownloadManager)
+                       .userDownloadsDirectory;
+  file.append(fileLeaf);
+  return file.path;
+}
+
+
+function printWithCanvas() {
+  var canvas = document.createElementNS("http://www.w3.org/1999/xhtml",
+                                        "canvas");
+
+  var canvasWidth = content.scrollMaxX + content.innerWidth;
+  var canvasHeight = content.scrollMaxY + content.innerHeight;
+
+  /*
+     Remove offset from scrollbar width.
+
+     17px on WindowsXP, but this may depends on client theme or something.
+     I guess the real width would be 16, plus extra 1px border for drop-
+     -shadow.
+     XXX FIX ME!
+   */
+  if (content.scrollMaxX)
+    canvasHeight -= 17;
+
+  if (content.scrollMaxY)
+    canvasWidth -= 17;
+
+  canvas.style.width = canvasWidth + "px";
+  canvas.style.height = canvasHeight + "px";
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+
+
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  ctx.save();
+  ctx.scale(1, 1);
+  ctx.drawWindow(content, 0, 0, canvasWidth, canvasHeight,
+                 "rgb(128,128,128)");
+  ctx.restore();
+//  document.documentElement.appendChild(canvas);
+  savePNG(canvas, outputFilePath(2));
+}
+
+
+var gPrintProgressListener = {
+  onStateChange : function (aWebProgress,
+                            aRequest,
+                            aStateFlags,
+                            aStatus) {
+    delayedShutdown();
+  },
+
+  onProgressChange : function (aWebProgress,
+                               aRequest,
+                               aCurSelfProgress,
+                               aMaxSelfProgress,
+                               aCurTotalProgress,
+                               aMaxTotalProgress){},
+  onLocationChange : function (aWebProgress,
+                               aRequest,
+                               aLocation){},
+
+  onStatusChange : function (aWebProgress,
+                             aRequest,
+                             aStatus,
+                             aMessage){},
+  onSecurityChange : function (aWebProgress,
+                               aRequest,
+                               aState){},
+
+  /* nsISupports */
+  QueryInterface : function progress_qi(aIID) {
+    if (!aIID.equals(Components.interfaces.nsISupports) &&
+        !aIID.equals(Components.interfaces.nsIWebProgressListener))
+      throw Components.results.NS_ERROR_NO_INTERFACE;
+
+    return this;
+  }
+};
+
+function delayedShutdown() {
+  window.setTimeout(window.close, 100);
+}
+
+function savePNG(aCanvas, aPath) {
+  var file = Components.classes["@mozilla.org/file/local;1"]
+                       .createInstance(Components.interfaces.nsILocalFile);
+  file.initWithPath(aPath);
+
+  var io = Components.classes["@mozilla.org/network/io-service;1"]
+                     .getService(Components.interfaces.nsIIOService);
+  var source = io.newURI(aCanvas.toDataURL("image/png", ""), null, null);
+  var target = io.newFileURI(file);
+    
+  // prepare to save the canvas data
+  var persist =
+    Components.classes["@mozilla.org/embedding/browser/nsWebBrowserPersist;1"]
+              .createInstance(Components.interfaces.nsIWebBrowserPersist);
+  
+  persist.persistFlags =
+    Components.interfaces.nsIWebBrowserPersist
+              .PERSIST_FLAGS_REPLACE_EXISTING_FILES |
+    Components.interfaces.nsIWebBrowserPersist
+              .PERSIST_FLAGS_AUTODETECT_APPLY_CONVERSION;
+  
+  persist.progressListener = gPrintProgressListener;
+  persist.saveURI(source, null, null, null, null, file);
 }
--- myapp/components/main.js
+++ myapp/components/main.js
@@ -1,77 +1,84 @@
-// constants
-const nsISupportsPriority = Components.interfaces.nsISupportsPriority;
-const nsISupports = Components.interfaces.nsISupports;
-const CLASS_ID = Components.ID("{1C0E8D86-B661-40d0-AE3D-CA012FADF177}");
-const CLASS_NAME = "My Supports Priority Component";
-const CONTRACT_ID = "@mozillazine.org/myapp/priority;1";
-
-//class constructor
-function MyPriority() {
-  this._priority = nsISupportsPriority.PRIORITY_LOWEST;
-};
-
-//class definition
-MyPriority.prototype = {
-  _priority: null,
-
-  get priority() { return this._priority; },
-  set priority(aValue) { this._priority = aValue; },
-
-  adjustPriority: function(aDelta) {
-    this._priority += aDelta;
-  },
-
-  QueryInterface: function(aIID)
-  {
-    if (!aIID.equals(nsISupportsPriority) &&    
-        !aIID.equals(nsISupports))
-      throw Components.results.NS_ERROR_NO_INTERFACE;
-    return this;
-  }
-};
-
-//class factory
-var MyPriorityFactory = {
-  createInstance: function (aOuter, aIID)
-  {
-    if (aOuter != null)
-      throw Components.results.NS_ERROR_NO_AGGREGATION;
-    return (new MyPriority()).QueryInterface(aIID);
-  }
-};
-
-//module definition (xpcom registration)
-var MyPriorityModule = {
-  _firstTime: true,
-  registerSelf: function(aCompMgr, aFileSpec, aLocation, aType)
-  {
-    if (this._firstTime) {
-      this._firstTime = false;
-      throw Components.results.NS_ERROR_FACTORY_REGISTER_AGAIN;
-    };
-    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
-    aCompMgr.registerFactoryLocation(CLASS_ID, CLASS_NAME, CONTRACT_ID, aFileSpec, aLocation, aType);
-  },
-
-  unregisterSelf: function(aCompMgr, aLocation, aType)
-  {
-    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
-    aCompMgr.unregisterFactoryLocation(CLASS_ID, aLocation);        
-  },
+const Cc = Components.classes;  
+const Ci = Components.interfaces;  
+const err_log = function(err){Components.utils.reportError(err);};
   
-  getClassObject: function(aCompMgr, aCID, aIID)
-  {
-    if (!aIID.equals(Components.interfaces.nsIFactory))
-      throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
-
-    if (aCID.equals(CLASS_ID))
-      return MyPriorityFactory;
-
-    throw Components.results.NS_ERROR_NO_INTERFACE;
-  },
-
-  canUnload: function(aCompMgr) { return true; }
-};
-
-//module initialization
-function NSGetModule(aCompMgr, aFileSpec) { return MyPriorityModule; }
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");  
+Components.utils.import("resource://gre/modules/Services.jsm");  
+  
+// CHANGEME: to the chrome URI of your extension or application  
+const CHROME_URI = "chrome://myapp/content/";  
+  
+/** 
+ * Utility functions 
+ */  
+  
+/** 
+ * Opens a chrome window. 
+ * @param aChromeURISpec a string specifying the URI of the window to open. 
+ * @param aArgument an argument to pass to the window (may be null) 
+ */  
+function openWindow(aChromeURISpec, aArgument)  
+{  
+  Services.ww.openWindow(null, aChromeURISpec, "_blank",  
+                "chrome,menubar,toolbar,status,resizable,dialog=no",  
+                aArgument);  
+}  
+  
+// Command Line Handler  
+function CommandLineHandler() {  
+};  
+  
+CommandLineHandler.prototype = {  
+  classDescription: "myAppHandler",  
+  // CHANGEME: generate a unique ID  
+  classID: Components.ID('{2991c315-b871-42cd-b33f-bfee4fcbf682}'),  
+  // CHANGEME: change the type in the contractID to be unique to your application  
+  contractID: "@mozilla.org/commandlinehandler/general-startup;1?type=myapp",  
+  _xpcom_categories: [{  
+    category: "command-line-handler",  
+    // CHANGEME:  
+    // category names are sorted alphabetically. Typical command-line handlers use a  
+    // category that begins with the letter "m".  
+    entry: "m-myapp"  
+  }],  
+  
+  QueryInterface: XPCOMUtils.generateQI([  
+    Ci.nsICommandLineHandler  
+  ]),  
+  
+  /* nsICommandLineHandler */  
+  handle : function clh_handle(cmdLine)  
+  {  
+    try {  
+      // CHANGEME: change "viewapp" to your command line flag that takes an argument  
+      var uristr = cmdLine.handleFlagWithParam("viewapp", false);  
+      if (uristr) {  
+        // convert uristr to an nsIURI  
+        var uri = cmdLine.resolveURI(uristr);  
+        openWindow(CHROME_URI, uri);  
+        cmdLine.preventDefault = true;  
+      }  
+    }  
+    catch (e) {  
+      Components.utils.reportError("incorrect parameter passed to -viewapp on the command line.");  
+    }  
+  
+    // CHANGEME: change "myapp" to your command line flag (no argument)  
+    if (cmdLine.handleFlag("myapp", false)) {  
+      openWindow(CHROME_URI, null);  
+      cmdLine.preventDefault = true;  
+    }  
+  },  
+  
+  // CHANGEME: change the help info as appropriate, but  
+  // follow the guidelines in nsICommandLineHandler.idl  
+  // specifically, flag descriptions should start at  
+  // character 24, and lines should be wrapped at  
+  // 72 characters with embedded newlines,  
+  // and finally, the string should end with a newline  
+  helpInfo : "  -myapp               Open My Application\n" +  
+             "  -viewapp <uri>       View and edit the URI in My Application,\n" +  
+             "                       wrapping this description\n"  
+};  
+  
+var NSGetFactory = XPCOMUtils.generateNSGetFactory([CommandLineHandler]); 
