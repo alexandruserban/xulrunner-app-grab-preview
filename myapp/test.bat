dd05979cca949abf93575aab8a232e4a51b2d1a0
alexandru.v.serban <alexandru.v.serban@95414121-e61c-0a86-1be5-034fa2485955>
2012-06-01 09:23:53 +0000

    text logging

--- myapp/test.bat
+++ myapp/test.bat
@@ -2,6 +2,6 @@
 
 FOR /L %%i IN (1,1,%1) DO (
   ECHO %%i
- start cmd.exe /C xulrunner application.ini -purgecaches -url %2 -width 1200 -height 1000 -time %%i
+ start /B cmd.exe /C xulrunner application.ini -purgecaches -url %2 -width 1200 -height 1000 -time %%i
 )
 	
